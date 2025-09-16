09942f953ab39c4d92a65755cd372dfd4e07ce13
michael-mccrickard <michael@mccrickard.net>
2025-09-16 12:10:16 -0400

    Update audio_manager.gd

--- audio_manager.gd
+++ audio_manager.gd
@@ -75,36 +75,6 @@ func _process(delta):
 	processVoice()
 	pass
 
-func processMic():
-	if effect == null:
-		return
-	var frames_available := effect.get_frames_available()
-	if frames_available <= 0:
-		return
-	var sterioData : PackedVector2Array = effect.get_buffer(frames_available)
-	if !talk_mode or partner_id == 0:
-		return
-
-	if sterioData.size() > 0:
-
-		var data = PackedFloat32Array()
-		data.resize(sterioData.size())
-		var maxAmplitude := 0.0
-
-		for i in range(sterioData.size()):
-			var value = (sterioData[i].x + sterioData[i].y) / 2
-			maxAmplitude = max(value, maxAmplitude)
-			data[i] = value
-		if maxAmplitude < inputThreshold:
-			return
-		#print("sending data from " + str(multiplayer.get_unique_id()))
-		# Just send the raw PCM floats:
-		var p = 0
-		while p < data.size():
-			var chunk_size = min(AUDIO_PACKET_SAMPLES, data.size() - p)
-			var chunk = data.slice(p, p + chunk_size)
-			sendData.rpc_id(partner_id, chunk)
-			p += chunk_size
 
 func processVoice():
 	if receiveBuffer.size() <= 0:
