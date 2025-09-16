a2ff45befe96b9a4c318416b3bf23e26d4764724
michael-mccrickard <mikemccrickard@gmail.com>
2025-09-16 10:16:36 -0400

    Add partner tracking and push-to-talk support

--- Client.gd
+++ Client.gd
@@ -7,6 +7,7 @@ var server_ip = "192.168.1.170"
 var peer = ENetMultiplayerPeer.new()
 @export var playerScene : PackedScene
 var serverIsReady : bool
+var partner_id : int = 0
 
 @export var gameSpawnLocation : NodePath
 # Called when the node enters the scene tree for the first time.
@@ -26,23 +27,31 @@ func _process(delta):
 
 func peerConnected(id):
 
-	print("peer connected! " + str(id))
-	#var p = playerScene.instantiate()
-	#add_child(p)
-	#p.name = str(id)
-	#p.get_node("AudioManager").setupAudio(id)
+        print("peer connected! " + str(id))
+        if id != multiplayer.get_unique_id():
+                partner_id = id
+                AudioManager.set_partner_id(partner_id)
+        #var p = playerScene.instantiate()
+        #add_child(p)
+        #p.name = str(id)
+        #p.get_node("AudioManager").setupAudio(id)
 
 func peerDisconnected(id):
-	print("peer disconnected! " + str(id))
+        print("peer disconnected! " + str(id))
+        if id == partner_id:
+                partner_id = 0
+                AudioManager.clear_partner_id()
 
 func _on_connect_to_server_pressed():
-	var error = peer.create_client(server_ip, 8910)
-	if error:
-		print("we have an error for client: " + error)
-	multiplayer.multiplayer_peer = peer
-	$"../Status".text = "You are connected to the app, id = " + str(multiplayer.get_unique_id())
-	
-	AudioManager.setupAudio(multiplayer.get_unique_id())
+        var error = peer.create_client(server_ip, 8910)
+        if error:
+                print("we have an error for client: " + error)
+        multiplayer.multiplayer_peer = peer
+        $"../Status".text = "You are connected to the app, id = " + str(multiplayer.get_unique_id())
+
+        partner_id = 0
+        AudioManager.clear_partner_id()
+        AudioManager.setupAudio(multiplayer.get_unique_id())
 
 	#var p = playerScene.instantiate()
 	#get_node(gameSpawnLocation).add_child(p)
--- Server.gd
+++ Server.gd
@@ -3,6 +3,7 @@ extends Node
 var peer = ENetMultiplayerPeer.new()
 @export var playerScene : PackedScene
 var serverIsReady : bool
+var partner_id : int = 0
 
 @export var gameSpawnLocation : NodePath
 # Called when the node enters the scene tree for the first time.
@@ -24,16 +25,23 @@ func _process(delta):
 
 func peerConnected(id):
 
-	print("peer connected! " + str(id))
+        print("peer connected! " + str(id))
+        partner_id = id
+        AudioManager.set_partner_id(partner_id)
 
 func peerDisconnected(id):
-	print("peer disconnected! " + str(id))
+        print("peer disconnected! " + str(id))
+        if id == partner_id:
+                partner_id = 0
+                AudioManager.clear_partner_id()
 
 func _on_host_button_down():
-	var error = peer.create_server(8910)
-	if error:
-		print("we have an error for server: " + error)
-	multiplayer.multiplayer_peer = peer
-	$"../Status".text = "You are hosting the app, id = " + str(multiplayer.get_unique_id())
-	
-	AudioManager.setupAudio(1)
+        var error = peer.create_server(8910)
+        if error:
+                print("we have an error for server: " + error)
+        multiplayer.multiplayer_peer = peer
+        $"../Status".text = "You are hosting the app, id = " + str(multiplayer.get_unique_id())
+
+        partner_id = 0
+        AudioManager.clear_partner_id()
+        AudioManager.setupAudio(1)
