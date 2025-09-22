507f15eff4cde60612e1ce3deabcea50e06dc1e1
michael-mccrickard <michael@mccrickard.net>
2025-09-22 16:08:30 -0400

    Client.gd

--- Client.gd
+++ Client.gd
@@ -12,42 +12,42 @@ var _connecting_to_local := false
 var _audio_initialized := false
 
 func _ready():
-        multiplayer.peer_connected.connect(peerConnected)
-        multiplayer.peer_disconnected.connect(peerDisconnected)
-        multiplayer.connected_to_server.connect(_on_connected_to_server)
-        multiplayer.connection_failed.connect(_on_connection_failed)
-        multiplayer.server_disconnected.connect(_on_server_disconnected)
+		multiplayer.peer_connected.connect(peerConnected)
+		multiplayer.peer_disconnected.connect(peerDisconnected)
+		multiplayer.connected_to_server.connect(_on_connected_to_server)
+		multiplayer.connection_failed.connect(_on_connection_failed)
+		multiplayer.server_disconnected.connect(_on_server_disconnected)
 
 func connect_client_to_server():
-        var args = OS.get_cmdline_args()
-        _connecting_to_local = "--local" in args
-        var server_ip
-        if _connecting_to_local:
-                server_ip = server_ip_local
-        else:
-                server_ip = server_ip_remote
+		var args = OS.get_cmdline_args()
+		_connecting_to_local = "--local" in args
+		var server_ip
+		if _connecting_to_local:
+				server_ip = server_ip_local
+		else:
+				server_ip = server_ip_remote
 
-        var conn_type
-        if _connecting_to_local:
-                conn_type = "local"
-        else:
-                conn_type = "remote"
+		var conn_type
+		if _connecting_to_local:
+				conn_type = "local"
+		else:
+				conn_type = "remote"
 
-        peer = ENetMultiplayerPeer.new()
-        var error = peer.create_client(server_ip, server_port)
-        if error == OK:
-                multiplayer.multiplayer_peer = peer
-                partner_id = 0
-                AudioManager.clear_partner_id()
-                _audio_initialized = false
-                showStatus("Attempting connection to %s server at %s:%d..." % [conn_type, server_ip, server_port])
-        else:
-                showStatus("Failed to create client peer: %s" % error)
+		peer = ENetMultiplayerPeer.new()
+		var error = peer.create_client(server_ip, server_port)
+		if error == OK:
+				multiplayer.multiplayer_peer = peer
+				partner_id = 0
+				AudioManager.clear_partner_id()
+				_audio_initialized = false
+				showStatus("Attempting connection to %s server at %s:%d..." % [conn_type, server_ip, server_port])
+		else:
+				showStatus("Failed to create client peer: %s" % error)
 
 func _process(delta):
-        if serverIsReady:
-                peer.poll()
-        pass
+		if serverIsReady:
+				peer.poll()
+		pass
 
 func peerConnected(id):
 	print("peer connected! " + str(id))
@@ -62,31 +62,31 @@ func peerDisconnected(id):
 		AudioManager.clear_partner_id()
 		
 func _on_connect_to_server_pressed():
-        connect_client_to_server()
+		connect_client_to_server()
 
 func showStatus(_str: String):
-        $"../Status".text = _str
+		$"../Status".text = _str
 
 func _on_connected_to_server():
-        var unique_id := multiplayer.get_unique_id()
-        partner_id = 0
-        AudioManager.clear_partner_id()
-        if !_audio_initialized:
-                AudioManager.setupAudio(unique_id)
-                _audio_initialized = true
+		var unique_id := multiplayer.get_unique_id()
+		partner_id = 0
+		AudioManager.clear_partner_id()
+		if !_audio_initialized:
+				AudioManager.setupAudio(unique_id)
+				_audio_initialized = true
 
-        if _connecting_to_local:
-                showStatus("You're connected to the local app, id = " + str(unique_id))
-        else:
-                showStatus("Connected to the remote server, id = " + str(unique_id))
+		if _connecting_to_local:
+				showStatus("You're connected to the local app, id = " + str(unique_id))
+		else:
+				showStatus("Connected to the remote server, id = " + str(unique_id))
 
 func _on_connection_failed():
-        showStatus("Failed to connect to server.")
-        partner_id = 0
-        AudioManager.clear_partner_id()
+		showStatus("Failed to connect to server.")
+		partner_id = 0
+		AudioManager.clear_partner_id()
 
 func _on_server_disconnected():
-        showStatus("Disconnected from server.")
-        partner_id = 0
-        AudioManager.clear_partner_id()
-        _audio_initialized = false
+		showStatus("Disconnected from server.")
+		partner_id = 0
+		AudioManager.clear_partner_id()
+		_audio_initialized = false
